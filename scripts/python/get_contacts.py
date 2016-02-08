0bd156bcb275cb4f22701927e1809ac91d7874c7
Rene Vogt-Lowell <rene.vogt-lowell@emc.com>
2016-02-08 08:03:58 -0500

    Cleanup for next internal tester

--- scripts/python/get_contacts.py
+++ scripts/python/get_contacts.py
@@ -1,6 +1,10 @@
-
 import requests
 r = requests.get("http://localhost:8080/ETAPP-REST-1/contacts/")
 print r.status_code
 print r.json()
+#import urllib2
+
+#url = 'http://localhost:8080/ETAPP-REST-1/contacts/'
+#response = urllib2.urlopen(url).read()
+#print response
 
